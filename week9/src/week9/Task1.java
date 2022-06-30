f4b7f474d37cc9dd5b89428ce19efc25cf8063a9
Jose <sanchezmartinezjosecarlos@gmail.com>
2022-06-30 16:55:04 +0100

    Task1 completed - pending Stretch 2

---
+++ week9/src/week9/Task1.java
@@ -0,0 +1,25 @@
+package week9;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Creates Scanner object
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Enter a number");
+		// Reads user number/input
+		int userNumber = userInput.nextInt();
+		// Reads number of iterations
+		System.out.println("Enter number of iterations");
+		int userIterations = userInput.nextInt();
+		System.out.println("The user number is " + userNumber + ", and will iterate " + userIterations + " times");
+		// Loops and displays times-table for the userNumber up to the number of userIterations
+		for (int i=0; i < userIterations; i++) {
+			int timesTable = userNumber * i;	
+		System.out.println(userNumber + " * " + i + " = " + timesTable);
+		
+		}
+	}
+}
